5fd0e7b2a3824dc77405ed141af8ab1f2a3d6094
Cihan Bal <afraidofdark@gmail.com>
2020-01-18 18:38:31 +0300

    C# Dropped. New editor will be GTK

--- cpydll.bat
+++
@@ -1,6 +0,0 @@
-xcopy /d /y "%~dp0\Bin\ToolKit.*" "%~dp0\Editor\bin\Release_Editor\netcoreapp3.0"
-xcopy /d /y "%~dp0\Bin\ToolKit_d.*" "%~dp0\Editor\bin\Debug_Editor\netcoreapp3.0"
-cd "%~dp0\Editor\bin\Debug_Editor\netcoreapp3.0"
-del ToolKit.*
-ren ToolKit_d.* ToolKit.*
-
