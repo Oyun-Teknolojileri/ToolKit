ddb841c876e84e0f85eaa3d523414ca429c17ff5
Cihan Bal <afraidofdark@gmail.com>
2020-02-12 00:26:38 +0300

    imagebutton is done

--- Editor/OverlayMenu.cpp
+++ Editor/OverlayMenu.cpp
@@ -10,10 +10,10 @@ ToolKit::Editor::OverlayNav::OverlayNav(Viewport* owner)
 
 void ToolKit::Editor::OverlayNav::ShowOverlayNav()
 {
-	ImVec2 overlaySize(300, 100);
+	ImVec2 overlaySize(48, 258);
 	ImVec2 padding = ImGui::GetStyle().WindowPadding;
 	const float headerSize = 17.0f;
-	ImVec2 window_pos = ImVec2(m_owner->m_wndPos.x + m_owner->m_wndContentAreaSize.x - overlaySize.x + padding.x, m_owner->m_wndPos.y + headerSize + padding.y);
+	ImVec2 window_pos = ImVec2(m_owner->m_wndPos.x + padding.x + 5, m_owner->m_wndPos.y + headerSize + padding.y + 5);
 	ImGui::SetNextWindowPos(window_pos);
 	ImGui::SetNextWindowBgAlpha(0.65f);
 	if (ImGui::BeginChildFrame(ImGui::GetID("Navigation"), overlaySize, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav))
@@ -22,18 +22,35 @@ void ToolKit::Editor::OverlayNav::ShowOverlayNav()
 		float spacing = style.ItemInnerSpacing.x;
 
 		// Select button.
-		static float hoverTimeX = 0.0f;
-		ImGui::Button("X"); ImGui::SameLine(); EditorGUI::HelpMarker("Camera speed m/s\n", &hoverTimeX); ImGui::SameLine(0, spacing);
+		static float hoverTimeSelectBtn = 0.0f;
+		ImGui::ImageButton((void*)(intptr_t)EditorGUI::m_selectIcn->m_textureId, ImVec2(32, 32));
+		EditorGUI::HelpMarker("Select Box\nSelect items using box selection.", &hoverTimeSelectBtn);
 
-		static float hoverTimeY = 0.0f;
-		ImGui::Button("Y"); ImGui::SameLine(); EditorGUI::HelpMarker("Camera speed m/s\n", &hoverTimeY); ImGui::SameLine(0, spacing);
+		// Cursor button.
+		static float hoverTimeCursorBtn = 0.0f;
+		ImGui::ImageButton((void*)(intptr_t)EditorGUI::m_cursorIcn->m_textureId, ImVec2(32, 32));
+		EditorGUI::HelpMarker("Cursor\nSet the cursor location. Drag to transform.", &hoverTimeCursorBtn);
+		ImGui::Separator();
 
-		static float hoverTimeZ = 0.0f;
-		ImGui::Button("Z"); ImGui::SameLine(); EditorGUI::HelpMarker("Camera speed m/s\n", &hoverTimeZ); ImGui::SameLine(0, spacing);
+		// Move button.
+		static float hoverTimeMoveBtn = 0.0f;
+		ImGui::ImageButton((void*)(intptr_t)EditorGUI::m_moveIcn->m_textureId, ImVec2(32, 32));
+		EditorGUI::HelpMarker("Move\nMove selected items.", &hoverTimeMoveBtn);
 
-		const char* items[] = { "0.5", "1", "2", "4", "8" };
-		static int current_item = 4;
-		ImGui::PushItemWidth(50);
+		// Rotate button.
+		static float hoverTimeRotateBtn = 0.0f;
+		ImGui::ImageButton((void*)(intptr_t)EditorGUI::m_rotateIcn->m_textureId, ImVec2(32, 32));
+		EditorGUI::HelpMarker("Rotate\nRotate selected items.", &hoverTimeRotateBtn);
+
+		// Scale button.
+		static float hoverTimeScaleBtn = 0.0f;
+		ImGui::ImageButton((void*)(intptr_t)EditorGUI::m_scaleIcn->m_textureId, ImVec2(32, 32));
+		EditorGUI::HelpMarker("Scale\nScale (resize) selected items.", &hoverTimeScaleBtn);
+		ImGui::Separator();
+
+		const char* items[] = {"1", "2", "4", "8" };
+		static int current_item = 2;
+		ImGui::PushItemWidth(40);
 		if (ImGui::BeginCombo("##CS", items[current_item], ImGuiComboFlags_None))
 		{
 			for (int n = 0; n < IM_ARRAYSIZE(items); n++)
