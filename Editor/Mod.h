5a89829af37b1f0669eed20bc753116f549e0196
Cihan Bal <afraidofdark@gmail.com>
2020-02-29 21:49:54 +0300

    Mods will be implemented as states of a state machine. ToolKit Icon is revised. NanoSvg removed. Not needed currently.

--- Editor/Mod.h
+++ Editor/Mod.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ToolKit.h"
+#include "StateMachine.h"
 
 namespace ToolKit
 {
@@ -9,20 +10,39 @@ namespace ToolKit
 		class BaseMod
 		{
 		public:
+			BaseMod();
+			virtual ~BaseMod();
+
 			virtual void Init();
-			virtual void StateBegin();
-			virtual void StateProcess();
-			virtual void StateEnd();
+			virtual void Update(float deltaTime_ms);
 
 		public:
-			int m_state; // While state is 0. It remains
+			StateMachine* m_stateMachine;
+		};
+
+		// Common signals and states.
+		class LeftClickSgnl : public SignalId
+		{
+		public:
+			LeftClickSgnl() : SignalId(101) {}
+		};
+
+		class LeftClickDragSgnl : public SignalId
+		{
+		public:
+			LeftClickDragSgnl() : SignalId(102) {}
+		};
+
+		class StatePicking : public State
+		{
+
 		};
 
 		class SelectMod : public BaseMod
 		{
 		public:
 			virtual void Init() override;
-
+			virtual void Update(float deltaTime_ms) override;
 		};
 	}
 }
