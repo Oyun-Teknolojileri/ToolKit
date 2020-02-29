eb2067cdc3be22b686ec1f4d81545f180bbdc495
Cihan Bal <afraidofdark@gmail.com>
2020-02-29 20:16:30 +0300

    mod wip

--- Editor/Mod.h
+++ Editor/Mod.h
@@ -10,12 +10,18 @@ namespace ToolKit
 		{
 		public:
 			virtual void Init();
+			virtual void StateBegin();
+			virtual void StateProcess();
+			virtual void StateEnd();
 
+		public:
+			int m_state; // While state is 0. It remains
 		};
 
 		class SelectMod : public BaseMod
 		{
 		public:
+			virtual void Init() override;
 
 		};
 	}
