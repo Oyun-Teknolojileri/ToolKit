abd7b2c35a859c78e5ebf62d0577d7beb8c21041
Cihan Bal <afraidofdark@gmail.com>
2020-04-20 18:57:39 +0300

    node revision, translate - rotate - scale works in same logic.

--- Source/Node.cpp
+++ Source/Node.cpp
@@ -61,13 +61,15 @@ namespace ToolKit
 		{
 		case TransformationSpace::TS_WORLD:
 		{
-			Quaternion ps, ws;
-			ws = GetOrientation(TransformationSpace::TS_WORLD);
+			glm::mat3 ps, ws;
+			ws = GetTransform(TransformationSpace::TS_WORLD);
 			if (m_parent != nullptr)
 			{
-				ps = m_parent->GetOrientation(TransformationSpace::TS_WORLD);
+				ps = m_parent->GetTransform(TransformationSpace::TS_WORLD);
 			}
-			m_orientation = glm::inverse(ps) * val * ws;
+
+			glm::mat3 ts = glm::inverse(ps) * glm::toMat3(val) * ws;
+			m_orientation = glm::toQuat(ts);
 		}
 		break;
 		case TransformationSpace::TS_PARENT:
@@ -75,13 +77,14 @@ namespace ToolKit
 			break;
 		case TransformationSpace::TS_LOCAL:
 		{
-			Quaternion ps, ws;
-			ws = GetOrientation(TransformationSpace::TS_WORLD);
+			glm::mat3 ps, ws;
+			ws = GetTransform(TransformationSpace::TS_WORLD);
 			if (m_parent != nullptr)
 			{
-				ps = m_parent->GetOrientation(TransformationSpace::TS_WORLD);
+				ps = m_parent->GetTransform(TransformationSpace::TS_WORLD);
 			}
-			m_orientation = glm::inverse(ps) * ws * val;
+			glm::mat3 ts = glm::inverse(ps) * ws * glm::toMat3(val);
+			m_orientation = glm::toQuat(ts);
 		}
 		}
 
