3ed557a9bf3b723febb91679a092104b4ab64b12
Cihan Bal <afraidofdark@gmail.com>
2022-11-01 00:18:44 +0300

    version update

--- ToolKit/Types.h
+++ ToolKit/Types.h
@@ -289,6 +289,6 @@ namespace ToolKit
     TargetCubeMap        = 0x8513
   };
 
-  static const char* TKVersionStr = "v0.3.16";
+  static const char* TKVersionStr = "v0.3.17";
 
 } // namespace ToolKit
