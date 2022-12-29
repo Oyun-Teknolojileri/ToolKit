080da991d3a1ba5035989b9325a4541ef2089804
Cihan Bal <afraidofdark@gmail.com>
2022-12-29 15:16:39 +0300

    v0.3.19

--- ToolKit/Types.h
+++ ToolKit/Types.h
@@ -319,6 +319,6 @@ namespace ToolKit
     AllBits          = ColorBits | DepthBits | StencilBits
   };
 
-  static const char* TKVersionStr = "v0.3.18";
+  static const char* TKVersionStr = "v0.3.19";
 
 } // namespace ToolKit
