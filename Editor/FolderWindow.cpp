10fe68f553f15f6cbd4f91673ef51585ba90566b
benanil <anilcangulkaya7@gmail.com>
2023-04-01 01:30:50 +0300

    less padding for folder node

--- Editor/FolderWindow.cpp
+++ Editor/FolderWindow.cpp
@@ -968,7 +968,7 @@ namespace ToolKit
       String icon         = node.active ? ICON_FA_FOLDER_OPEN : ICON_FA_FOLDER;
       String nodeHeader   = icon + ICON_SPACE + node.name;
       float headerLen     = ImGui::CalcTextSize(nodeHeader.c_str()).x; 
-      headerLen          += (depth * 20.0f) + 100.0f; // depth padding + UI start padding
+      headerLen          += (depth * 20.0f) + 90.0f; // depth padding + UI start padding
       m_maxTreeNodeWidth  = glm::max(headerLen, m_maxTreeNodeWidth);
       
       const auto onClickedFn = [&]() -> void
