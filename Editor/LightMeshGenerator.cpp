dc239fce1e71e781a2e14390ff6ab27402d9920f
Cihan Bal <afraidofdark@gmail.com>
2023-05-14 16:23:14 +0300

    refactor lights, memory leak & billboard problems to be fixed.

---
+++ Editor/LightMeshGenerator.cpp
@@ -0,0 +1,292 @@
+#include "LightMeshGenerator.h"
+
+#include "EditorLight.h"
+#include "Global.h"
+
+namespace ToolKit
+{
+  namespace Editor
+  {
+
+    // LightMeshGenerator
+    //////////////////////////////////////////////////////////////////////////
+
+    LightMeshGenerator::LightMeshGenerator(Light* light)
+    {
+      m_targetLight = light;
+
+      m_lightMesh   = std::make_shared<MeshComponent>();
+      m_lightMesh->SetCastShadowVal(false);
+      m_lightMesh->ParamMesh().m_exposed       = false;
+      m_lightMesh->ParamCastShadow().m_exposed = false;
+    }
+
+    LightMeshGenerator::~LightMeshGenerator() { m_targetLight = nullptr; }
+
+    void LightMeshGenerator::TransferGizmoMesh(LineBatchRawPtrArray& lines)
+    {
+      if (lines.empty())
+      {
+        return;
+      }
+
+      MeshPtr accum = lines[0]->GetMeshComponent()->GetMeshVal();
+      lines[0]->GetMeshComponent()->SetMeshVal(nullptr);
+
+      for (int i = 1; i < (int) lines.size(); i++)
+      {
+        LineBatch* lb   = lines[i];
+        MeshPtr subMesh = lb->GetMeshComponent()->GetMeshVal();
+        accum->m_subMeshes.push_back(subMesh);
+        lb->GetMeshComponent()->SetMeshVal(nullptr);
+
+        SafeDel(lb);
+      }
+
+      lines.clear();
+      m_lightMesh->SetMeshVal(accum);
+    }
+
+    // SpotLightMeshGenerator
+    //////////////////////////////////////////////////////////////////////////
+
+    SpotLightMeshGenerator::SpotLightMeshGenerator(SpotLight* light)
+        : LightMeshGenerator(light)
+    {
+    }
+
+    void SpotLightMeshGenerator::InitGizmo()
+    {
+      assert(m_targetLight->GetType() == EntityType::Entity_SpotLight);
+
+      // Middle line.
+      SpotLight* sLight = static_cast<SpotLight*>(m_targetLight);
+
+      float r           = sLight->GetRadiusVal();
+      Vec3 d            = Vec3(0.0f, 0.0f, -1.0f);
+
+      m_pnts[0]         = Vec3(ZERO);
+      m_pnts[1]         = Vec3(d * r * 2.25f);
+
+      m_innerCirclePnts.reserve(m_circleVertexCount + 1);
+      m_outerCirclePnts.reserve(m_circleVertexCount + 1);
+
+      LineBatchRawPtrArray lines = {new LineBatch(),
+                                    new LineBatch(),
+                                    new LineBatch()};
+
+      // Calculating circles.
+      int zeroCount              = 0;
+      if (d.x == 0.0f)
+      {
+        zeroCount++;
+      }
+      if (d.y == 0.0f)
+      {
+        zeroCount++;
+      }
+      if (d.z == 0.0f)
+      {
+        zeroCount++;
+      }
+
+      Vec3 per;
+      if (zeroCount == 0)
+      {
+        per.z = 0.0f;
+        per.x = -d.y;
+        per.y = d.x;
+      }
+      else if (zeroCount == 1)
+      {
+        if (d.x == 0.0f)
+        {
+          per.x = 0.0f;
+          per.y = -d.z;
+          per.z = d.y;
+        }
+        else if (d.y == 0.0f)
+        {
+          per.y = 0.0f;
+          per.x = -d.z;
+          per.z = d.x;
+        }
+        else
+        {
+          per.z = 0.0f;
+          per.y = -d.x;
+          per.x = d.y;
+        }
+      }
+      else if (zeroCount == 2)
+      {
+        if (d.x == 0.0f && d.y == 0.0f)
+        {
+          per.x = 1.0f;
+          per.y = 0.0f;
+          per.z = 0.0f;
+        }
+        else if (d.x == 0.0f && d.z == 0.0f)
+        {
+          per.x = 1.0f;
+          per.y = 0.0f;
+          per.z = 0.0f;
+        }
+        else
+        {
+          per.x = 0.0f;
+          per.y = 1.0f;
+          per.z = 0.0f;
+        }
+      }
+
+      per = glm::normalize(per);
+      d   = d * r;
+
+      float innerCircleRadius =
+          r * glm::tan(glm::radians(sLight->GetInnerAngleVal() / 2));
+
+      float outerCircleRadius =
+          r * glm::tan(glm::radians(sLight->GetOuterAngleVal() / 2));
+
+      Vec3 inStartPoint  = d + per * innerCircleRadius;
+      Vec3 outStartPoint = d + per * outerCircleRadius;
+
+      m_innerCirclePnts.push_back(inStartPoint);
+      m_outerCirclePnts.push_back(outStartPoint);
+
+      float deltaAngle = glm::two_pi<float>() / m_circleVertexCount;
+
+      for (int i = 1; i < m_circleVertexCount + 1; i++)
+      {
+        // Inner circle vertices.
+        m_rot        = glm::rotate(m_identityMatrix, deltaAngle, d);
+        inStartPoint = Vec3(m_rot * Vec4(inStartPoint, 1.0f));
+        m_innerCirclePnts.push_back(inStartPoint);
+
+        // Outer circle vertices.
+        m_rot         = glm::rotate(m_identityMatrix, deltaAngle, d);
+        outStartPoint = Vec3(m_rot * Vec4(outStartPoint, 1.0f));
+        m_outerCirclePnts.push_back(outStartPoint);
+      }
+
+      lines[0]->Generate(m_innerCirclePnts,
+                         g_lightGizmoColor,
+                         DrawType::LineStrip,
+                         1.0f);
+
+      lines[1]->Generate(m_outerCirclePnts,
+                         g_lightGizmoColor,
+                         DrawType::LineStrip,
+                         1.0f);
+
+      // Cone
+      m_conePnts.resize(2 * (m_circleVertexCount / 4));
+
+      int coneIndex = 0;
+      for (int i = 0; i < m_circleVertexCount; i += 4)
+      {
+        m_conePnts[coneIndex]     = ZERO;
+        m_conePnts[coneIndex + 1] = m_outerCirclePnts[i];
+        coneIndex                 += 2;
+      }
+
+      lines[2]->Generate(m_conePnts, g_lightGizmoColor, DrawType::Line, 1.0f);
+
+      TransferGizmoMesh(lines);
+    }
+
+    // DirectionalLightMeshGenerator
+    //////////////////////////////////////////////////////////////////////////
+
+    DirectionalLightMeshGenerator::DirectionalLightMeshGenerator(
+        DirectionalLight* light)
+        : LightMeshGenerator(light)
+    {
+    }
+
+    void DirectionalLightMeshGenerator::InitGizmo()
+    {
+      assert(m_targetLight->GetType() == EntityType::Entity_DirectionalLight);
+
+      // Middle line
+      Vec3 d    = Vec3(0.0f, 0.0f, -1.0f);
+      Vec3 norm = glm::normalize(d);
+
+      m_pnts.resize(2);
+      m_pnts[0] = Vec3(ZERO);
+      m_pnts[1] = Vec3(d * 10.0f);
+
+      LineBatchRawPtrArray lines {new LineBatch()};
+      lines[0]->Generate(m_pnts, g_lightGizmoColor, DrawType::Line, 1.0f);
+
+      TransferGizmoMesh(lines);
+    }
+
+    // PointLightMeshGenerator
+    //////////////////////////////////////////////////////////////////////////
+
+    PointLightMeshGenerator::PointLightMeshGenerator(PointLight* light)
+        : LightMeshGenerator(light)
+    {
+    }
+
+    void PointLightMeshGenerator::InitGizmo()
+    {
+      assert(m_targetLight->GetType() == EntityType::Entity_PointLight);
+
+      m_circlePntsXY.resize(m_circleVertexCount + 1);
+      m_circlePntsYZ.resize(m_circleVertexCount + 1);
+      m_circlePntsXZ.resize(m_circleVertexCount + 1);
+
+      Vec3 up            = Vec3(0.0f, 1.0f, 0.0f);
+      Vec3 right         = Vec3(1.0f, 0.0f, 0.0f);
+      Vec3 forward       = Vec3(0.0f, 0.0f, 1.0f);
+      float deltaAngle   = glm::two_pi<float>() / m_circleVertexCount;
+
+      PointLight* pLight = static_cast<PointLight*>(m_targetLight);
+
+      Vec3 lightPos =
+          pLight->m_node->GetTranslation(TransformationSpace::TS_WORLD);
+
+      LineBatchRawPtrArray lines = {new LineBatch(),
+                                    new LineBatch(),
+                                    new LineBatch()};
+
+      auto drawCircleGizmoFn =
+          [&pLight, &deltaAngle, &lines, this](Vec3Array vertices,
+                                               const Vec3& axis,
+                                               const Vec3& perpAxis,
+                                               int lineBatchIndex)
+      {
+        Vec3 startingPoint  = perpAxis * pLight->GetRadiusVal();
+        vertices[0]         = startingPoint;
+        Mat4 idendityMatrix = Mat4(1.0f);
+
+        for (int i = 1; i < m_circleVertexCount + 1; ++i)
+        {
+          Mat4 rot      = glm::rotate(idendityMatrix, deltaAngle, axis);
+          startingPoint = Vec3(rot * Vec4(startingPoint, 1.0f));
+          vertices[i]   = startingPoint;
+        }
+
+        lines[lineBatchIndex]->Generate(vertices,
+                                        g_lightGizmoColor,
+                                        DrawType::LineStrip,
+                                        1.0f);
+      };
+
+      // Circle on XY plane
+      drawCircleGizmoFn(m_circlePntsXY, forward, up, 0);
+
+      // Circle on YZ plane
+      drawCircleGizmoFn(m_circlePntsXY, right, up, 1);
+
+      // Circle on XZ plane
+      drawCircleGizmoFn(m_circlePntsXY, up, right, 2);
+
+      TransferGizmoMesh(lines);
+    }
+
+  } // namespace Editor
+} // namespace ToolKit
---
+++ Editor/LightMeshGenerator.h
@@ -0,0 +1,74 @@
+#pragma once
+
+namespace ToolKit
+{
+  namespace Editor
+  {
+
+    class LightMeshGenerator
+    {
+     public:
+      LightMeshGenerator(Light* light);
+      ~LightMeshGenerator();
+
+      virtual void InitGizmo() = 0;
+
+      /**
+       * Constructs a MeshComponent from the given lines. Destroys
+       * all LineBatch objects in the lines array and clears the array.
+       * @param lines is the line batch array that contains generated gizmo
+       * data.
+       */
+      void TransferGizmoMesh(LineBatchRawPtrArray& lines);
+
+     public:
+      MeshComponentPtr m_lightMesh; //!< Component that contains generated data.
+
+     protected:
+      Light* m_targetLight = nullptr; //!< Light to generate mesh for.
+    };
+
+    class SpotLightMeshGenerator : public LightMeshGenerator
+    {
+     public:
+      explicit SpotLightMeshGenerator(SpotLight* light);
+      void InitGizmo() override;
+
+     private:
+      const int m_circleVertexCount = 36;
+
+      Mat4 m_identityMatrix;
+      Mat4 m_rot;
+
+      Vec3 m_pnts[2];
+      Vec3Array m_innerCirclePnts;
+      Vec3Array m_outerCirclePnts;
+      Vec3Array m_conePnts;
+    };
+
+    class DirectionalLightMeshGenerator : public LightMeshGenerator
+    {
+     public:
+      explicit DirectionalLightMeshGenerator(DirectionalLight* light);
+      void InitGizmo() override;
+
+     private:
+      Vec3Array m_pnts;
+    };
+
+    class PointLightMeshGenerator : public LightMeshGenerator
+    {
+     public:
+      explicit PointLightMeshGenerator(PointLight* light);
+      void InitGizmo() override;
+
+     private:
+      const int m_circleVertexCount = 30;
+
+      Vec3Array m_circlePntsXY;
+      Vec3Array m_circlePntsYZ;
+      Vec3Array m_circlePntsXZ;
+    };
+
+  } // namespace Editor
+} // namespace ToolKit
