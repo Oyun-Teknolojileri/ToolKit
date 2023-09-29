6c9d4f3410dcd43a3083a9192253177f452d1429
Osman Fatih Çakır <cakirosmanfatih@gmail.com>
2023-09-29 16:37:56 +0300

    fix render job that has 0 vertices to render
    the cursor mesh was corrupted, fixed

--- ToolKit/Renderer.cpp
+++ ToolKit/Renderer.cpp
@@ -203,11 +203,6 @@ namespace ToolKit
     Mesh* mesh = job.Mesh;
     activateSkinning(mesh->IsSkinned());
 
-    if (mesh->m_vertexCount == 0)
-    {
-      return;
-    }
-
     RenderState* rs = m_mat->GetRenderState();
     SetRenderState(rs);
     FeedUniforms(prg);
