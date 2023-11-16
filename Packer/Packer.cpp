887007167e76a828de35f25a09004ed45f4358bb
DESKTOP-U5E0NCJ\Cihan <cihan@otyazilim.com>
2023-11-17 00:06:33 +0300

    update license for import and packer

--- Packer/Packer.cpp
+++ Packer/Packer.cpp
@@ -1,30 +1,12 @@
 /*
- * MIT License
- *
- * Copyright (c) 2019 - Present Cihan Bal - Oyun Teknolojileri ve Yazılım
- * https://github.com/Oyun-Teknolojileri
- * https://otyazilim.com/
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Copyright (c) 2019-2024 OtSofware
+ * This code is licensed under the GNU Lesser General Public License v3.0 (LGPL-3.0).
+ * For more information, including options for a more permissive commercial license,
+ * please visit [otyazilim.com] or contact us at [info@otyazilim.com].
  */
 
 #include <Animation.h>
+#include <Common/Win32Utils.h>
 #include <FileManager.h>
 #include <Material.h>
 #include <RenderSystem.h>
@@ -34,7 +16,6 @@
 #include <Texture.h>
 #include <ToolKit.h>
 #include <Util.h>
-#include <Common/Win32Utils.h>
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -678,7 +659,7 @@ namespace ToolKit
     }
     // Set resource root to project's Resources folder
     g_proxy->m_resourceRoot = ConcatPaths({workspacePath, activeProjectName, "Resources"});
-    
+
     String toolkitAppdata   = ConcatPaths({getenv("APPDATA"), "ToolKit", "Config", "Path.txt"});
     String toolkitPath      = GetFileManager()->ReadAllText(toolkitAppdata);
     NormalizePathInplace(toolkitPath);
