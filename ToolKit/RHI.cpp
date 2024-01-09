8b157ce16bc0b1a392fc4167598d6bf045949011
Cihan Bal <cihan@otyazilim.com>
2024-01-09 23:07:55 +0300

    ref rhi

--- ToolKit/RHI.cpp
+++ ToolKit/RHI.cpp
@@ -1,8 +1,88 @@
+/*
+ * Copyright (c) 2019-2024 OtSofware
+ * This code is licensed under the GNU Lesser General Public License v3.0 (LGPL-3.0).
+ * For more information, including options for a more permissive commercial license,
+ * please visit [otyazilim.com] or contact us at [info@otyazilim.com].
+ */
+
 #include "RHI.h"
 
 namespace ToolKit
 {
+
   GLuint RHI::m_currentReadFramebufferID = -1; // max unsigned integer
   GLuint RHI::m_currentDrawFramebufferID = -1; // max unsigned integer
   GLuint RHI::m_currentFramebufferID     = -1; // max unsigned integer
+
+  void RHI::SetFramebuffer(GLenum target, GLuint framebufferID)
+  {
+    if (target == GL_READ_FRAMEBUFFER)
+    {
+      if (m_currentReadFramebufferID == framebufferID)
+      {
+        return;
+      }
+      else
+      {
+        m_currentReadFramebufferID = framebufferID;
+      }
+    }
+    else if (target == GL_DRAW_FRAMEBUFFER)
+    {
+      if (m_currentDrawFramebufferID == framebufferID)
+      {
+        return;
+      }
+      else
+      {
+        m_currentDrawFramebufferID = framebufferID;
+      }
+    }
+    else
+    {
+      if (m_currentFramebufferID == framebufferID && m_currentReadFramebufferID == framebufferID &&
+          m_currentDrawFramebufferID == framebufferID)
+      {
+        return;
+      }
+      else
+      {
+        m_currentFramebufferID     = framebufferID;
+        m_currentReadFramebufferID = framebufferID;
+        m_currentDrawFramebufferID = framebufferID;
+      }
+    }
+
+    glBindFramebuffer(target, framebufferID);
+    AddHWRenderPass();
+  }
+
+  void RHI::DeleteFramebuffers(GLsizei n, const GLuint* framebuffers)
+  {
+    glDeleteFramebuffers(n, framebuffers);
+
+    for (int i = 0; i < n; ++i)
+    {
+      if (framebuffers[i] == m_currentFramebufferID)
+      {
+        m_currentFramebufferID = -1;
+      }
+
+      if (framebuffers[i] == m_currentReadFramebufferID)
+      {
+        m_currentReadFramebufferID = -1;
+      }
+
+      if (framebuffers[i] == m_currentDrawFramebufferID)
+      {
+        m_currentDrawFramebufferID = -1;
+      }
+    }
+  }
+
+  void RHI::InvalidateFramebuffer(GLenum target, GLsizei numAttachments, const GLenum* attachments)
+  {
+    glInvalidateFramebuffer(target, numAttachments, attachments);
+  }
+
 } // namespace ToolKit
--- ToolKit/RHI.h
+++ ToolKit/RHI.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2019-2024 OtSofware
+ * This code is licensed under the GNU Lesser General Public License v3.0 (LGPL-3.0).
+ * For more information, including options for a more permissive commercial license,
+ * please visit [otyazilim.com] or contact us at [info@otyazilim.com].
+ */
+
 #pragma once
 
 #include "TKOpenGL.h"
@@ -12,81 +19,15 @@ namespace ToolKit
     friend class Framebuffer;
     friend class RenderSystem;
 
-  private:
-      static GLuint m_currentReadFramebufferID;
-      static GLuint m_currentDrawFramebufferID;
-      static GLuint m_currentFramebufferID;
+   private:
+    static void SetFramebuffer(GLenum target, GLuint framebufferID);
+    static void DeleteFramebuffers(GLsizei n, const GLuint* framebuffers);
+    static void InvalidateFramebuffer(GLenum target, GLsizei numAttachments, const GLenum* attachments);
 
    private:
-    static inline void SetFramebuffer(GLenum target, GLuint framebufferID)
-    {
-      if (target == GL_READ_FRAMEBUFFER)
-      {
-        if (m_currentReadFramebufferID == framebufferID)
-        {
-          return;
-        }
-        else
-        {
-          m_currentReadFramebufferID = framebufferID;
-        }
-      }
-      else if (target == GL_DRAW_FRAMEBUFFER)
-      {
-        if (m_currentDrawFramebufferID == framebufferID)
-        {
-          return;
-        }
-        else
-        {
-          m_currentDrawFramebufferID = framebufferID;
-        }
-      }
-      else
-      {
-        if (m_currentFramebufferID == framebufferID && m_currentReadFramebufferID == framebufferID &&
-            m_currentDrawFramebufferID == framebufferID)
-        {
-          return;
-        }
-        else
-        {
-          m_currentFramebufferID     = framebufferID;
-          m_currentReadFramebufferID = framebufferID;
-          m_currentDrawFramebufferID = framebufferID;
-        }
-      }
-
-      glBindFramebuffer(target, framebufferID);
-      AddHWRenderPass();
-    }
-
-    static inline void DeleteFramebuffers(GLsizei n, const GLuint* framebuffers)
-    {
-      glDeleteFramebuffers(n, framebuffers);
-
-      for (int i = 0; i < n; ++i)
-      {
-        if (framebuffers[i] == m_currentFramebufferID)
-        {
-          m_currentFramebufferID = -1;
-        }
-
-        if (framebuffers[i] == m_currentReadFramebufferID)
-        {
-          m_currentReadFramebufferID = -1;
-        }
-
-        if (framebuffers[i] == m_currentDrawFramebufferID)
-        {
-          m_currentDrawFramebufferID = -1;
-        }
-      }
-    }
-
-    static inline void InvalidateFramebuffer(GLenum target, GLsizei numAttachments, const GLenum* attachments)
-    {
-      glInvalidateFramebuffer(target, numAttachments, attachments);
-    }
+    static GLuint m_currentReadFramebufferID;
+    static GLuint m_currentDrawFramebufferID;
+    static GLuint m_currentFramebufferID;
   };
+
 } // namespace ToolKit
