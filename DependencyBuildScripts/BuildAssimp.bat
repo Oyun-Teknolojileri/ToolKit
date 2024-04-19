a315b933ac07d7ba2f4c4fb9a4f86f526eadba89
Cihan Bal <cihan@otyazilim.com>
2024-04-19 11:32:23 +0300

    assimp debug dlls added

--- DependencyBuildScripts/BuildAssimp.bat
+++ DependencyBuildScripts/BuildAssimp.bat
@@ -12,3 +12,16 @@ copy "assimp-vc143-mt.pdb" "../../../../../Utils/Import/assimp-vc143-mt.pdb"
 cd "../../lib/Release"
 
 copy "assimp-vc143-mt.lib" "../../../../../Import/assimp-vc143-mt.lib"
+
+cd "../../"
+
+cmake --build . --config Debug
+
+cd "bin/Debug"
+
+copy "assimp-vc143-mtd.dll" "../../../../../Utils/Import/assimp-vc143-mtd.dll"
+copy "assimp-vc143-mtd.pdb" "../../../../../Utils/Import/assimp-vc143-mtd.pdb"
+
+cd "../../lib/Debug"
+
+copy "assimp-vc143-mtd.lib" "../../../../../Import/assimp-vc143-mtd.lib"
