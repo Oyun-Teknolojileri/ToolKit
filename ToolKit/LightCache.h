8634e19ae66e437ab8606c461d0e69ca778c9049
Osman Fatih Çakır <osman@otyazilim.com>
2024-04-19 18:39:11 +0300

    fix linker error

--- ToolKit/LightCache.h
+++ ToolKit/LightCache.h
@@ -12,18 +12,51 @@ namespace ToolKit
   class TK_API LightCache
   {
    public:
-    LightCache();
-    ~LightCache();
-    void Add(Light* light);
-    void Reset();
+    LightCache() { Reset(); }
+
+    ~LightCache() { m_leastFreqUsedLightIndices.clear(); }
+
+    inline void Add(Light* light)
+    {
+      uint lfuIndex = m_leastFreqUsedLightIndices.front();
+      m_leastFreqUsedLightIndices.pop_front();
+
+      m_lightCache[lfuIndex] = light;
+      m_leastFreqUsedLightIndices.push_back(lfuIndex);
+
+      UpdateVersion();
+    }
+
+    void Reset()
+    {
+      m_leastFreqUsedLightIndices.clear();
+      for (uint i = 0; i < T; ++i)
+      {
+        m_leastFreqUsedLightIndices.push_back(i);
+        m_lightCache[i] = nullptr;
+      }
+      m_lightCacheVersion++;
+    }
 
     // returns the index when found, returns -1 when not found
-    int Contains(const Light* light);
+    int Contains(const Light* light)
+    {
+      for (int i = 0; i < T; ++i)
+      {
+        if (m_lightCache[i] == light)
+        {
+          return i;
+        }
+      }
 
-    uint16_t GetVersion() const;
-    void UpdateVersion();
+      return -1;
+    }
 
-    Light** GetLights();
+    uint16_t GetVersion() const { return m_lightCacheVersion; }
+
+    void UpdateVersion() { m_lightCacheVersion++; }
+
+    Light** GetLights() { return m_lightCache; }
 
    private:
     Light* m_lightCache[T];
