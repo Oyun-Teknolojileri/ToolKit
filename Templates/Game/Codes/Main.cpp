83d1bb3f48c21e193247be76fae522b29a12e898
Cihan Bal <cihan@otyazilim.com>
2024-07-13 21:58:11 +0300

    no depth stencil for back buffer in game

--- Templates/Game/Codes/Main.cpp
+++ Templates/Game/Codes/Main.cpp
@@ -78,8 +78,9 @@ namespace ToolKit
       SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
       SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 
-      SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-      SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+      SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+      SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 0);
+      SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 0);
 
       // EGL does not support sRGB backbuffer. Need to use an extension
       // https://stackoverflow.com/questions/20396523/android-egl-srgb-default-renderbuffer
