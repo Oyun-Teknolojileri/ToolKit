03813ef7d63b858f450677f48b09b81b6cc35ef3
Cihan Bal <cihan@otyazilim.com>
2024-08-24 15:57:54 +0300

    comment update

--- ToolKit/Prefab.h
+++ ToolKit/Prefab.h
@@ -27,28 +27,22 @@ namespace ToolKit
     Prefab();
     virtual ~Prefab();
 
-    /**
-     * Initiates prefab scene and link to the current scene.
-     */
+    /** Initiates prefab scene and link to the current scene. */
     void Init(Scene* currentScene);
 
-    /**
-     * Destroys all prefab scene entities and unlink.
-     */
+    /** Destroys all prefab scene entities and unlink. */
     void UnInit();
 
-    /**
-     * Remove the prefab entity and everything inside the prefab scene from
-     * the current scene.
-     */
+    /** Remove the prefab entity and everything inside the prefab scene from the current scene. */
     void Unlink();
 
-    /**
-     * Add all elements in the prefab scene to the current scene.
-     */
+    /** Add all elements in the prefab scene to the current scene. */
     void Link();
 
+    /** If the entity is child of a prefab, returns the prefab entity. */
     static PrefabPtr GetPrefabRoot(const EntityPtr ntt);
+
+    /** Copy the prefab. New prefab is initialized but not linked by default. */
     Entity* CopyTo(Entity* other) const override;
 
     /**
@@ -56,6 +50,7 @@ namespace ToolKit
      * @return First entity with given name. Null pointer if the entity is not found or the prefab is not linked.
      */
     EntityPtr GetFirstByName(const String& name);
+
     /**
      * This function will look for the first entity with given tag in LINKED scene.
      * @return First entity with given tag. Null pointer if the entity is not found or the prefab is not linked.
