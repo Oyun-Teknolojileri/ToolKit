9ff997cf3e9ae43c05f9396f03760906435004db
Cihan Bal <cihan@otyazilim.com>
2024-12-14 17:04:14 +0300

    touch fixes for android template

--- Templates/Game/Android/app/jni/src/android_main.h
+++ Templates/Game/Android/app/jni/src/android_main.h
@@ -5,11 +5,11 @@
  * please visit [otyazilim.com] or contact us at [info@otyazilim.com].
  */
 
+#include "EngineSettings.h"
 #include "Game.h"
 #include "Logger.h"
 #include "SDL.h"
 #include "Util.h"
-#include "EngineSettings.h"
 
 #include <android/asset_manager.h>
 #include <android/asset_manager_jni.h>
@@ -23,7 +23,7 @@
 
 #define ANDROID_LOG(format, ...) __android_log_print(ANDROID_LOG_DEBUG, "TK_LOG", format, ##__VA_ARGS__)
 
-#define PLATFORM_SDL_FLAGS (SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN)
+#define PLATFORM_SDL_FLAGS       (SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN)
 
 #define TK_PLATFORM              PLATFORM::TKAndroid
 
@@ -40,8 +40,8 @@ namespace ToolKit
       ANDROID_LOG("cannot open MinResources.pak!\n");
       return;
     }
-    FILE* fileHandle = fopen(ConcatPaths({ internalDataPath, file }).c_str(), "wb");
-    off_t size = AAsset_getLength(asset);
+    FILE* fileHandle = fopen(ConcatPaths({internalDataPath, file}).c_str(), "wb");
+    off_t size       = AAsset_getLength(asset);
     std::vector<char> buffer;
     buffer.resize(size + 1, '\0');
     AAsset_read(asset, buffer.data(), size);
@@ -54,8 +54,8 @@ namespace ToolKit
   // copies all of the engine assets to internal data folder if already not copied
   inline void CopyAllAssetsToDataPath(String& internalDataPath)
   {
-    mkdir(ConcatPaths({ internalDataPath, "Resources" }).c_str(), 0777);
-    mkdir(ConcatPaths({ internalDataPath, "Config" }).c_str(), 0777);
+    mkdir(ConcatPaths({internalDataPath, "Resources"}).c_str(), 0777);
+    mkdir(ConcatPaths({internalDataPath, "Config"}).c_str(), 0777);
 
     CopyFileToDataPath(internalDataPath, "MinResources.pak");
     CopyFileToDataPath(internalDataPath, ConcatPaths({"Config", "Engine.settings"}).c_str());
@@ -63,10 +63,10 @@ namespace ToolKit
 
   inline void PlatformPreInit(Main* g_proxy)
   {
-    String internalPath = SDL_AndroidGetInternalStoragePath();
+    String internalPath     = SDL_AndroidGetInternalStoragePath();
 
-    g_proxy->m_resourceRoot = ConcatPaths({ internalPath, "Resources" });
-    g_proxy->m_cfgPath = ConcatPaths({ internalPath, "Config"});
+    g_proxy->m_resourceRoot = ConcatPaths({internalPath, "Resources"});
+    g_proxy->m_cfgPath      = ConcatPaths({internalPath, "Config"});
 
     // Set log function
     GetLogger()->SetWriteConsoleFn([](LogType lt, String ms) -> void { ANDROID_LOG("%s", ms.c_str()); });
@@ -84,8 +84,8 @@ namespace ToolKit
 
   inline void PlatformAdjustEngineSettings(int availableWidth, int availableHeight, EngineSettings* engineSettings)
   {
-    engineSettings->Window.Width = (uint)availableWidth;
-    engineSettings->Window.Height = (uint)availableHeight;
+    engineSettings->Window.Width  = (uint) availableWidth;
+    engineSettings->Window.Height = (uint) availableHeight;
   }
 } // namespace ToolKit
 
