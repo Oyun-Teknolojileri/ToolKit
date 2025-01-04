3e6f9b85dd6aa517344b2382846eae31da691c23
Cihan Bal <cihan@otyazilim.com>
2025-01-04 18:21:41 +0300

    Override sdl activity with toolkit activity

---
+++ Templates/Game/Android/app/src/main/java/com/otyazilim/toolkit/ToolKitActivity.java
@@ -0,0 +1,22 @@
+package com.otyazilim.toolkit;
+
+import android.content.res.AssetManager;
+import android.os.Bundle;
+
+import org.libsdl.app.SDLActivity;
+
+public class ToolKitActivity extends SDLActivity
+{
+    // this function is implemented in cpp
+    private static native void load(AssetManager mgr);
+
+    public void loadAssetManagerToCpp(AssetManager mgr) {
+        load(mgr);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super::onCreate(savedInstanceState);
+        loadAssetManagerToCpp(getResources().getAssets());
+    }
+}
