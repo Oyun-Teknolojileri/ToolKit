eae7b7b481da410109329661b1ce4ca063127f83
Cihan Bal <cihan@otyazilim.com>
2025-01-19 22:55:53 +0300

    splash

--- ToolKit/SplashScreenRenderPath.h
+++ ToolKit/SplashScreenRenderPath.h
@@ -3,6 +3,7 @@
 #include "ForwardPass.h"
 #include "GammaTonemapFxaaPass.h"
 #include "RenderSystem.h"
+#include "Scene.h"
 #include "UIManager.h"
 #include "Viewport.h"
 
@@ -16,12 +17,28 @@ namespace ToolKit
     virtual ~SpashScreenRenderPath() {};
 
     /** Initialize all resources that will be used to render splash screen. */
-    void Init()
+    void Init(UVec2 screenSize)
     {
       m_uiPass       = MakeNewPtr<ForwardRenderPass>();
       m_gammaPass    = MakeNewPtr<GammaTonemapFxaaPass>();
-      m_viewport     = MakeNewPtr<Viewport>();
+      m_viewport     = MakeNewPtr<Viewport>((float) screenSize.x, (float) screenSize.y);
       m_splashScreen = MakeNewPtr<UILayer>(LayerPath("ToolKit/splash-screen.layer"));
+
+      if (UIManager* uiMan = GetUIManager())
+      {
+        uiMan->RegisterViewport(m_viewport);
+        uiMan->AddLayer(m_viewport->m_viewportId, m_splashScreen);
+        m_uiPass->m_params.Cam = uiMan->GetUICamera();
+      }
+
+      m_uiPass->m_params.FrameBuffer = m_viewport->m_framebuffer;
+    }
+
+    void PreRender(Renderer* renderer) override
+    {
+      EntityRawPtrArray rawEntities = ToEntityRawPtrArray(m_splashScreen->m_scene->GetEntities());
+      RenderJobProcessor::CreateRenderJobs(m_uiRenderData.jobs, rawEntities);
+      RenderJobProcessor::SeperateRenderData(m_uiRenderData, true);
     }
 
    private:
@@ -29,6 +46,7 @@ namespace ToolKit
     GammaTonemapFxaaPassPtr m_gammaPass = nullptr;
     ViewportPtr m_viewport              = nullptr;
     UILayerPtr m_splashScreen           = nullptr;
+    RenderData m_uiRenderData;
   };
 
 } // namespace ToolKit
